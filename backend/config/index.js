e52cc471b3781a96c5150b9e03db425c79b36581
munendra <munendrashiv97@gmail.com>
2024-03-17 13:34:13 +0530

    .

--- backend/config/index.js
+++ backend/config/index.js
@@ -7,7 +7,6 @@ const BACKEND_SERVER_PATH = process.env.BACKEND_SERVER_PATH;
 const CLOUD_NAME = process.env.CLOUD_NAME;
 const API_KEY = process.env.API_KEY;
 const API_SECRET = process.env.API_SECRET;
-
 module.exports = {
   PORT,
   MONGODB_CONNECTION_STRING,
--- backend/server.js
+++ backend/server.js
@@ -6,18 +6,19 @@ const errorHandler = require("./middlewares/errorHandler");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const app = express();
+
 app.use(cookieParser());
-app.use(cors(
-  {
-    origin: [" "],
-    method: ["POST", "GET"],
-    credentials: true
-  }
-));
+
+app.use(cors({
+  origin: "*", // Allow requests from any origin
+  methods: ["POST", "GET"], // Allow the specified methods
+  credentials: true
+}));
 
 app.use(express.json({ limit: "50mb" }));
 app.use(router);
 dbConnect();
 app.use("/storage", express.static("storage"));
 app.use(errorHandler);
+
 app.listen(PORT, console.log(`Backend is running on port: ${PORT}`));
